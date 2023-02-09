20049ba01be2e793e9c6c9b505054d0b1b19edb5
SBUX <sinsunkangg@naver.com>
2023-02-09 11:19:30 +0900

    aa

---
+++ src/main/java/kr/co/kmarket2/dao/MemberDAO.java
@@ -0,0 +1,21 @@
+package kr.co.kmarket2.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import kr.co.kmarket2.vo.MemberVO;
+import kr.co.kmarket2.vo.TermsVO;
+
+
+@Mapper
+@Repository
+public interface MemberDAO {
+	public int insertMember(MemberVO vo);
+	public MemberVO selectMember(String uid);
+	public List<MemberVO> selectMembers();
+	public void updateMember(MemberVO vo);
+	public void deleteMember(String uid);
+  public TermsVO selectTerms();
+}
---
+++ src/main/java/kr/co/kmarket2/service/MemberService.java
@@ -0,0 +1,55 @@
+package kr.co.kmarket2.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.kmarket2.dao.MemberDAO;
+import kr.co.kmarket2.vo.MemberVO;
+import kr.co.kmarket2.vo.TermsVO;
+
+@Service
+public class MemberService {
+
+	@Autowired
+	private MemberDAO dao;
+//@Autowired
+//private MemberRepo repo;
+	
+	public void insertMember(MemberVO vo) {
+		dao.insertMember(vo);
+	}
+	public MemberVO selectMember(String uid) {
+		return dao.selectMember(uid);
+	}
+	public List<MemberVO> selectMembers(){
+		return dao.selectMembers();
+	}
+	public void updateMember(MemberVO vo) {
+		dao.updateMember(vo);
+	}
+	public void deleteMember(String uid) {
+		dao.deleteMember(uid);
+	}
+  
+  // 약관
+	public TermsVO selectTerms() {
+		return dao.selectTerms();
+	}
+	
+	
+//	// 회원가입
+//	public int insertMember(MemberVO vo) {
+//		vo.setPass(PasswordEncoder.encode(vo.getPass()));
+//		
+//		int result = dao.insertMember(vo);
+//		
+//		return result;
+//	}
+	
+//	public int countMember(String uid) {
+//		return repo.countByUid(uid);
+//	}
+
+}
