25294281be1fa97b25bb13347877ddfadf56eb6f
SBUX <sinsunkangg@naver.com>
2023-02-21 13:44:59 +0900

    2023-02-21 강중현

--- src/main/java/kr/co/kmarket2/controller/MainController.java
+++ src/main/java/kr/co/kmarket2/controller/MainController.java
@@ -1,26 +1,27 @@
 package kr.co.kmarket2.controller;
 
+import java.security.Principal;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.co.kmarket2.repository.MemberRepo;
-import kr.co.kmarket2.service.ProductService;
-import kr.co.kmarket2.vo.ProductVO;
 
 @Controller
 public class MainController {
 	
-	@Autowired
-	ProductService service;
 	@Autowired
 	private MemberRepo repo;
 	
 	
 	@GetMapping(value = {"/", "index"})
     public String index(Model model){
-
+		
+		
+		
         return "index";
     }
 	
